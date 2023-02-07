fd432123ad326e61dbc99302ac734fefac1bed78
E-RI-CK <74332121+E-RI-CK@users.noreply.github.com>
2023-02-07 17:54:28 -0500

    mobile responsive added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const d = document,
+  w = window;
 //Permite mostrar el menú lateral del header en dispositivos moviles
 
 let navToggle = document.querySelector(".nav-toggle");
@@ -30,3 +32,52 @@ if (document.querySelector('#container-slider')) {
   setInterval('fntExecuteSlide("next")', 5000);
 }
 
+
+// /*****************Slider */
+// var windowWidth = w.innerWidth;
+// w.addEventListener("load", () => {
+//   let slide = 1;
+//   let $slides = d.querySelectorAll(".combos .contenedor-combo5"),
+//     total = $slides.length,
+//     $next = d.querySelector("#flechaDerecha3"),
+//     $prev = d.querySelector("#flechaIzquierda3");
+//   console.log($slides, total);
+//   if (windowWidth <= 1024) {
+//     showSlide(1);
+//     $next.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       slide++;
+//       console.log($slides, total);
+//       if (slide > total) { slide == 1; }
+//       showSlide(slide);
+//     });
+
+//     $prev.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       slide--;
+
+//       if (slide < 1) { slide == total; }
+//       showSlide(slide);
+//     });
+
+//     function showSlide(n) {
+//       n--; // decrement 1
+//       for (i = 0; i < total; i++) {
+//         (i == n) ? $slides[n].style.display = "block" : $slides[i].style.display = "none";
+//       }
+//     }
+//   }
+  // if(windowWidth > 1024) {
+  //   const carrusel = document.querySelector("#carrusel-combos");
+
+  //   /* Permite el funcionamiento del carrusel especialess */
+  //   const carrusel3 = document.querySelector("#carrusel-combos3");
+  //   const flechaIzq3 = document.querySelector("#flechaIzquierda3");
+  //   const flechaDer3 = document.querySelector("#flechaDerecha3");
+  //   flechaDer3.addEventListener("click", () => {
+  //     carrusel3.scrollLeft += carrusel3.offsetWidth;
+  //   });
+  //   flechaIzq3.addEventListener("click", () => {
+  //     carrusel3.scrollLeft -= carrusel3.offsetWidth;
+  //   });
+  // }
