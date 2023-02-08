16ee4208187a9085d3a557c992b0bcbfd7639071
E-RI-CK <74332121+E-RI-CK@users.noreply.github.com>
2023-02-07 22:11:40 -0500

    contact section to desktop finished

--- src/main.js
+++ src/main.js
@@ -11,73 +11,78 @@ navToggle.addEventListener("click", () => {
 
 
 /* Permite el funcionamiento del carrusel Pollos  */
-const carrusel = document.querySelector("#carrusel-combos");
+// const carrusel = document.querySelector("#carrusel-combos");
 
-/* Permite el funcionamiento del carrusel especialess */
-const carrusel3 = document.querySelector("#carrusel-combos3");
-const flechaIzq3 = document.querySelector("#flechaIzquierda3");
-const flechaDer3 = document.querySelector("#flechaDerecha3");
-flechaDer3.addEventListener("click", () => {
-  carrusel3.scrollLeft += carrusel3.offsetWidth;
-});
-flechaIzq3.addEventListener("click", () => {
-  carrusel3.scrollLeft -= carrusel3.offsetWidth;
-});
+// /* Permite el funcionamiento del carrusel especialess */
+// const carrusel3 = document.querySelector("#carrusel-combos3");
+// const flechaIzq3 = document.querySelector("#flechaIzquierda3");
+// const flechaDer3 = document.querySelector("#flechaDerecha3");
+// flechaDer3.addEventListener("click", () => {
+//   carrusel3.scrollLeft += carrusel3.offsetWidth;
+// });
+// flechaIzq3.addEventListener("click", () => {
+//   carrusel3.scrollLeft -= carrusel3.offsetWidth;
+// });
 /*<<<<<<<<<<<<<------>>>>>>>>>>  */
 
 /* Permite el funcionamiento del carrusel especialess */
 
 
-if (document.querySelector('#container-slider')) {
-  setInterval('fntExecuteSlide("next")', 5000);
-}
+// if (document.querySelector('#container-slider')) {
+//   setInterval('fntExecuteSlide("next")', 5000);
+// }
 
 
 // /*****************Slider */
-// var windowWidth = w.innerWidth;
-// w.addEventListener("load", () => {
-//   let slide = 1;
-//   let $slides = d.querySelectorAll(".combos .contenedor-combo5"),
-//     total = $slides.length,
-//     $next = d.querySelector("#flechaDerecha3"),
-//     $prev = d.querySelector("#flechaIzquierda3");
-//   console.log($slides, total);
-//   if (windowWidth <= 1024) {
-//     showSlide(1);
-//     $next.addEventListener("click", (e) => {
-//       e.preventDefault();
-//       slide++;
-//       console.log($slides, total);
-//       if (slide > total) { slide == 1; }
-//       showSlide(slide);
-//     });
+var windowWidth = w.innerWidth;
 
-//     $prev.addEventListener("click", (e) => {
-//       e.preventDefault();
-//       slide--;
+if (windowWidth <= 1024) {
+  w.addEventListener("load", () => {
+    let slide = 1;
+    let $slides = d.querySelectorAll(".combos .contenedor-combo5"),
+      total = $slides.length,
+      $next = d.querySelector("#flechaDerecha3"),
+      $prev = d.querySelector("#flechaIzquierda3");
+    // console.log($slides, total);
 
-//       if (slide < 1) { slide == total; }
-//       showSlide(slide);
-//     });
+    showSlide(1);
+    $next.addEventListener("click", (e) => {
+      e.preventDefault();
+      slide++;
+      // console.log($slides, total);
+      if (slide > total) { slide = 1; }
+      showSlide(slide);
+    });
 
-//     function showSlide(n) {
-//       n--; // decrement 1
-//       for (i = 0; i < total; i++) {
-//         (i == n) ? $slides[n].style.display = "block" : $slides[i].style.display = "none";
-//       }
-//     }
-//   }
-  // if(windowWidth > 1024) {
-  //   const carrusel = document.querySelector("#carrusel-combos");
+    $prev.addEventListener("click", (e) => {
+      e.preventDefault();
+      slide--;
 
-  //   /* Permite el funcionamiento del carrusel especialess */
-  //   const carrusel3 = document.querySelector("#carrusel-combos3");
-  //   const flechaIzq3 = document.querySelector("#flechaIzquierda3");
-  //   const flechaDer3 = document.querySelector("#flechaDerecha3");
-  //   flechaDer3.addEventListener("click", () => {
-  //     carrusel3.scrollLeft += carrusel3.offsetWidth;
-  //   });
-  //   flechaIzq3.addEventListener("click", () => {
-  //     carrusel3.scrollLeft -= carrusel3.offsetWidth;
-  //   });
-  // }
+      if (slide < 1) { slide = total; }
+      showSlide(slide);
+    });
+
+    function showSlide(n) {
+      n--; // decrement 1
+      for (i = 0; i < total; i++) {
+        (i == n) ? $slides[n].style.display = "block" : $slides[i].style.display = "none";
+      }
+    }
+  });
+}
+
+if (windowWidth > 1024) {
+  // const carrusel = document.querySelector("#carrusel-combos");
+  w.addEventListener("load", () => {
+    /* Permite el funcionamiento del carrusel especialess */
+    const carrusel3 = document.querySelector("#carrusel-combos3");
+    const flechaIzq3 = document.querySelector("#flechaIzquierda3");
+    const flechaDer3 = document.querySelector("#flechaDerecha3");
+    flechaDer3.addEventListener("click", () => {
+      carrusel3.scrollLeft += carrusel3.offsetWidth;
+    });
+    flechaIzq3.addEventListener("click", () => {
+      carrusel3.scrollLeft -= carrusel3.offsetWidth;
+    });
+  });
+};
